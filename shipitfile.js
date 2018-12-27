9ee236ac37559aca523ad6fdb3db937f0bdd1e43
qbylucky <18600211621@163.com>
2018-12-27 11:51:13 +0800

    pm2 config

---
+++ shipitfile.js
@@ -0,0 +1,31 @@
+module.exports = function (shipit) {
+    require('shipit-deploy')(shipit);
+    require('shipit-npm')(shipit);
+    require('shipit-pm2')(shipit);
+  
+    shipit.initConfig({
+      default: {
+        workspace: __dirname,
+        deployTo: '/var/www/website',
+        repositoryUrl: 'https://github.com/QETHAN/jxadmin.git',
+        ignores: ['.git', 'node_modules'],
+        keepReleases: 2,
+        deleteOnRollback: false,
+        key: '/Users/qinbinyang/.ssh/id_rsa',
+        shallowClone: true,
+        pm2: {
+            json: 'app.json'
+        },
+        shared: {
+            overwrite: true,
+            files: [
+                'config/application.yml',
+                'config/database.yml'
+            ],
+        }
+      },
+      staging: {
+        servers: 'ubuntu@129.28.82.67'
+      }
+    });
+};
